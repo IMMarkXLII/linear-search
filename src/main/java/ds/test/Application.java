e85d2d7aa578b89bc3828e5bd030ae88287e994a
ashish.adhikari@vodafone.com <ashish.adhikari@vodafone.com>
2018-07-09 05:30:51 +0000

    linear search complete

--- src/main/java/ds/test/Application.java
+++ src/main/java/ds/test/Application.java
@@ -3,7 +3,24 @@ package ds.test;
 public class Application
 {
 	public static void main( String[] args )
-	{
+	{	
+		// init the array 
+		int arr[] = new int[] {3, 5, 2, 1, 7, 8, 9, 4, 12, 10};
 		
+		int toFind = 4;
+		
+		int foundIndex = -1;
+		
+		for(int i = 0 ; i < arr.length ; i++) {
+			if(arr[i] == toFind) {
+				foundIndex = i; 
+				break;
+			}
+		}
+		
+		if(foundIndex >= 0)
+			System.out.println(toFind + " found at index " + foundIndex + " in the array!");
+		else
+			System.out.println(toFind + " does not exist in the array!");
 	}
 }
